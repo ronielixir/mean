3246c44bc2b206162c5e0ca8bc4ae7cb1343a26f
roniphire <roni@phirestudio.com>
2019-10-31 00:16:33 +0700

    add emiter test

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const greet3 = require('./greet3');
 const greet3b = require('./greet3');
 const Greet4 = require('./greet4');
 const greet5 = require('./greet5').greet;
+//const Emitter = require('./emitter');
+const Emitter = require('events');
+const eventConfig = require('./config').events;
 
 
 // Invoking greet from module greet
@@ -13,7 +16,7 @@ greet.english();
 greet.sunda();
 
 console.log('---------------------------------');
-console.log('module pattern avaliability');
+console.log('module pattern availability');
 console.log('---------------------------------');
 
 // Invoke function expression from export
@@ -22,7 +25,7 @@ greet1();
 //invoke object that already define in export
 greet2();
 
-// invoke created object fron export
+// invoke created object from export
 greet3.greet();
 greet3.greeting = 'Changed hello world!';
 greet3b.greet();
@@ -33,3 +36,12 @@ grtr.greet();
 
 // Revealing module pattern
 greet5();
+
+// Emitter
+const emtr = new Emitter();
+
+emtr.on(eventConfig.GREET, () => console.log('somewhere....'));
+emtr.on(eventConfig.GREET, () => console.log('....somehow'));
+
+console.log('Hi');
+emtr.emit(eventConfig.GREET);
